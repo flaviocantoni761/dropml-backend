117d99b7ab4a21a5213726b849f53222df8b4398
flaviocantoni761 <flaviocantoni761@gmail.com>
2026-05-14 16:39:39 -0300

    fix database url

--- server.js
+++ server.js
@@ -1005,3 +1005,4 @@ console.log(`   Dashboard:http://localhost:${PORT}/api/dashboard\n`);
 // redeploy Thu May 14 15:42:15     2026
 // reconnect Thu May 14 15:57:18     2026
 // upgrade
+// fix db url
